0e397b6c18950fbca6668f87a7f15a34ab847dad
Lucas Dos Santos Souza <lucassantospublico@gmail.com>
2024-08-04 12:56:25 -0300

    test / first commit code

--- behavior.py
+++ behavior.py
@@ -1,5 +1,5 @@
 import Range, random, copy, math # type: ignore
-from mathutils import Vector, Euler, noise # type: ignore
+from mathutils import Vector, Matrix, Euler, noise # type: ignore
 
 class Behavior():
     def __init__(self, storage, utils):
@@ -7,7 +7,7 @@ class Behavior():
         self.utils = utils
 
     # Getters
-    def getMouseOver(self, distance = 9999, scene = Range.logic.getCurrentScene()) -> object:
+    def getMouseOver(self, distance = 99999, scene = Range.logic.getCurrentScene()) -> object:
         """  """
 
         Mouse = Range.logic.mouse.position
@@ -23,6 +23,8 @@ class Behavior():
             return {}
         
     def getInstanceBy(self, key: str, array: list = [], value = None):
+        """  """
+
         finded = []
         for instance in array:
             if value != None:
@@ -33,7 +35,6 @@ class Behavior():
                     finded.append(instance)
         
         return (finded if len(finded) > 1 else finded[0] if finded else None)
-
 
     # Setters
     def SetValue(self, object: object, variable: str, value):
@@ -65,9 +66,11 @@ class Behavior():
         else:
             instance.worldPosition = target + Vector(offset[0])
 
-        return instance
+        return instance.worldPosition
     
-    def setLookAt(self, instance: object, target: list, axi: str = "Z"):
+    def setLookAt(self, instance: object, target: list, axi: str = "Z") -> Matrix:
+        """  """
+
         orientation = [
             target[0] - instance.worldPosition[0],
             target[1] - instance.worldPosition[1]
@@ -82,16 +85,30 @@ class Behavior():
             'XYZ'
         )
 
-    def SimpleMoviment(self, target: object):
+        return instance.worldOrientation
+
+    def CharacterMoviment(self, instance: object, speed: float = 0.01, useOrientation: bool = False) -> Vector:
+        """  """
+
+        CharacterWrapper = Range.constraints.getCharacter(instance)
+        if Range.logic.keyboard.inputs[Range.events.WKEY].active:
+            CharacterWrapper.walkDirection = Vector([0, speed, 0])
+
+        return instance.worldPosition
+
+    def SimpleMoviment(self, instance: object, speed: float = 1.0) -> Vector:
+        """  """
 
         if Range.logic.keyboard.inputs[Range.events.WKEY].active:
-            target.applyMovement([0.002,0,0], True)
+            instance.applyMovement([speed,0,0], True)
         elif Range.logic.keyboard.inputs[Range.events.SKEY].active:
-            target.applyMovement([-0.002,0,0], True)
+            instance.applyMovement([-speed,0,0], True)
         if Range.logic.keyboard.inputs[Range.events.AKEY].active:
-            target.applyMovement([0,0.002,0], True)
+            instance.applyMovement([0,speed,0], True)
         elif Range.logic.keyboard.inputs[Range.events.DKEY].active:
-            target.applyMovement([0,-0.002,0], True)
+            instance.applyMovement([0,-speed,0], True)
+        
+        return instance.worldPosition
 
     def SeaMoviment(self, target: object, config: bool = False) -> object:
         """  """
@@ -120,11 +137,13 @@ class Behavior():
 
             target['absoluteSpeed'] = target['relativeSpeed'] * 10000
 
-        return target
+        return target.worldPosition
     
     # Actuators
 
     def setCameraActuator(self, target: object, actuator: str, changes: list = []):
+        """  """
+
         if target.actuators.get(actuator):
             for function in changes:
                 if hasattr(target.actuators[actuator], function['name']):
